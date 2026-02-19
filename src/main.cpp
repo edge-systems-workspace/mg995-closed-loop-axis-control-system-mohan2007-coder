bbae1e39833007f6aad3d12fd19c47019cadf24f
mohan2007-coder <mohan.sharma_cs.aiml24@gla.ac.in>
2026-02-19 09:24:02 +0530

    initilise the trigpin and echopin

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
+/**
+* @file main.cpp
+ * @brief MG995 Closed-Loop Axis Control System
+ * @author Mohan Sharma
+ * @date YYYY-MM-DD
+ *
+ * @details
+ * Implements PWM-based closed-loop control for MG995 servo motor.
+ */
 #include <Arduino.h>
+#include<Servo.h>
+Servo myservo;
+int echopin=10;
+//initilise the trigpin and echopin
+int trigpin=9;
 void setup() {
-// write your initialization code here
+    pinMode(trigpin,OUTPUT);
+    pinMode(echopin,INPUT);
+    Serial.begin(9600);
+    myservo.attach(3);
+
+
 }
 
 
+
+
+
 void loop() {
-// write your code here
+    digitalWrite(trigpin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigpin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigpin,0);
+    long duration=pulseIn(echopin,1);
+    long distance=(duration*0.034)/2;
+    if(distance<15){
+        myservo.write(90);
+    }
+    else{
+        myservo.write(0);
+
+    }
+    Serial.print("Distance");
+    Serial.print(distance);
+    Serial.println("cm");
+    delay(500);
+    // put your main code here, to run repeatedly:
+
 }
